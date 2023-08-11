1abcb5d7aaacd1f7f15f61fd3b6117e6a9289fdc
Mohammad Tariq <fk68790@gmail.com>
2023-08-11 23:45:44 +0530

    origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: 'https://react-google-form.netlify.app' });
   const config = new DocumentBuilder()
     .setTitle('Google forms example')
     .setDescription('The google form API description')
