717ab302da02222ea2ad06959e90c68ace984096
Mohammad Tariq <fk68790@gmail.com>
2023-08-14 11:33:22 +0530

    vercel

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://react-google-form.netlify.app' });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   const config = new DocumentBuilder()
     .setTitle('Google forms example')
     .setDescription('The google form API description')
