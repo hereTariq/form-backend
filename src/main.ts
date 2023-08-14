859a025df4a3e9ef450ac4704d222371823a31c2
Mohammad Tariq <fk68790@gmail.com>
2023-08-14 11:38:37 +0530

    enablecors

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
 
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Google forms example')
     .setDescription('The google form API description')
